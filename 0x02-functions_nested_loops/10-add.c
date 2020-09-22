fa0e36c06e74a6b73e9a91df8592689224c2804b
Lunerio <lunerio@outlook.es>
2020-09-22 22:28:10 +0000

    asd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
