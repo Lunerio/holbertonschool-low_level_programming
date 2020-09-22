a7fa4d755c8e43523cc4bffaeceee58626720a84
Lunerio <lunerio@outlook.es>
2020-09-22 16:45:17 +0000

    repair 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,21 +6,16 @@
  * Return: Always 0.
  */
 
-int main(void)
-{
-	int r;
-
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-        print_last_digit(r);
-	return (0);
-}
-
-
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int ln;
+	ln = n % 10;
+
+	if (n < 0)
+	{
+		ln = ln * (-1);
+	}
+
+	_putchar(ln + '0');
+	return (ln);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,34 +1,38 @@
-#include "holberton.h"
+B#include "holberton.h"
 
 /**
  * jack_bauer - check the code for Holberton School students.
  * @a @b @c @d: to be declared
  * Return: Always 0.
  */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
 
 void jack_bauer(void)
 {
-	int a, b, c, d = 0;
+	int a, b, c, d;
 
-	while(a <= 2)
+	for (a = '0'; a <= '2'; a++)
 	{
-		while (b <= 9)
+		for  (b = '0'; b <= '9'; b++)
 		{
-			while (c <= 5)
+			for (c = '0'; c <= '5'; c++)
 			{
-				while(d <= 9)
+				for (d = '0'; d <= '9'; d++)
 				{
+					if (a == '2' && b == '3' && c == '5' && d == '9')
+						break;
 					_putchar(a);
 					_putchar(b);
 					_putchar(':');
 					_putchar(c);
 					_putchar(d);
-					d++;
-				}
-				c++;
+					_putchar('\n');
+				 }
 			}
-			b++;
 		}
-		a++;
 	}
 }
