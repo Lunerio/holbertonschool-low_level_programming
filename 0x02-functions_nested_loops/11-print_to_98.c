b7e9db4ad51372f0d0eec3cd99c0e6b4c8b02239
Lunerio <lunerio@outlook.es>
2020-09-22 23:23:48 +0000

    repair 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,18 +2,9 @@
 #include <stdio.h>
 /**
  * main - check the code for Holberton School students.
- *
+ * @n: to be declared
  * Return: Always 0.
  */
-int main(void)
-{
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return (0);
-}
 
 void print_to_98(int n)
 {
