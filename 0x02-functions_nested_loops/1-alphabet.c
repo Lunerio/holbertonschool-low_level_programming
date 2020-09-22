6ed397f25a8c525c4211352be0c3bf5973e4798f
Lunerio <lunerio@outlook.es>
2020-09-22 12:41:10 +0000

    repair task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
-	char l = 'a';
-	while (l <= 'z')
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
 	{
 		_putchar(l);
-		l++;
 	}
 }
