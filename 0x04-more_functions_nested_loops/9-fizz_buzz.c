99233accaa70f63a7f55a6314ee502296dd1ea38
Lunerio <lunerio@outlook.es>
2020-09-24 19:57:09 +0000

    repair 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,26 +4,27 @@ int main(void)
 {
 	int i;
 
-	i = 0;
-	while (i < 100)
+	i = 1;
+	while (i <= 100)
 	{
-		i++;
-		if (i % 3 == 0)
+		if (i % 15 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
+			printf("Buzz");
 		}
 		else
 		{
-		printf("%d ", i);
+		printf("%d", i);
 		}
+		i++;
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
