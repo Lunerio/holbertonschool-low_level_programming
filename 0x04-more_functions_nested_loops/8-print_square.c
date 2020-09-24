9616d22bc9e283bbdfdee4c4e4a1144d007985a8
Lunerio <lunerio@outlook.es>
2020-09-24 19:14:53 +0000

    repair 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,17 +1,10 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_square - check the code for Holberton School students.
+ * @size: to be declared
  * Return: Always 0.
  */
-int main(void)
-{
-	print_square(2);
-	print_square(10);
-	print_square(0);
-	return (0);
-}
 
 void print_square(int size)
 {
