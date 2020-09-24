88b9012638d69ca4094642708aa9319fecb27c1f
Lunerio <lunerio@outlook.es>
2020-09-24 20:33:11 +0000

    repair 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,14 +5,6 @@
  * @n: to be declared
  * Return: Always 0.
  */
-int main(void)
-{
-	print_diagonal(0);
-	print_diagonal(2);
-	print_diagonal(10);
-	print_diagonal(-4);
-	return (0);
-}
 
 void print_diagonal(int n)
 {
@@ -30,16 +22,13 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j < i; j++)
 			{
-				if (i == j)
+				if (j < i)
 				{
-				_putchar('\\');
-				_putchar('\n');
-				}
-				else
-				{
-				_putchar(' ');
+					_putchar(' ');
 				}
 			}
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
 }
