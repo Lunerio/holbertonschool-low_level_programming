bfb9a6825c80ef161ecd2a684cad5f2cb1555774
Lunerio <lunerio@outlook.es>
2020-09-24 15:13:56 +0000

    add files

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,10 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_line - check the code for Holberton School students.
+ * @n: to be declared
  * Return: Always 0.
  */
-int main(void)
-{
-	print_line(0);
-	print_line(2);
-	print_line(10);
-	print_line(-4);
-	return (0);
-}
 
 void print_line(int n)
 {
