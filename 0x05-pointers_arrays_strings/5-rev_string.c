7b5847e2bd40792f3f62eab34f6904989e3ca26a
Lunerio <lunerio@outlook.es>
2020-09-28 23:41:21 +0000

    add 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,3 +18,21 @@ int main(void)
 
 void rev_string(char *s)
 {
+	int min;
+	int max;
+
+	min = 0;
+	max = 0;
+
+	while (s[max] != '\0')
+	{
+		max++;
+	}
+
+	while ((min != '\0') && (max < 0))
+	{
+		min++;
+		max--;
+	}
+	_putchar('\n');
+}
