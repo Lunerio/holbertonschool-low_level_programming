1edd56abb08186194aecf76291e7bf636e21654e
Lunerio <lunerio@outlook.es>
2020-09-29 20:40:00 +0000

    repair 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,20 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strcpy - check the code for Holberton School students.
+ * @dest: pointer to dest
+ * @src: pointer to src
  * Return: Always 0.
  */
-int main(void)
-{
-	char s1[98];
-	char *p;
-
-	p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-	printf("%s", s1);
-	printf("%s", p);
-	return (0);
-}
 
 char *_strcpy(char *dest, char *src)
 {
