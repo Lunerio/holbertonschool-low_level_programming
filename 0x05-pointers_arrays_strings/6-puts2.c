7acabc14fe93b58f960567e9b1e0b34302c0de95
Lunerio <lunerio@outlook.es>
2020-09-29 00:16:04 +0000

    add 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts2 - check the code for Holberton School students.
+ * @str: 
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
