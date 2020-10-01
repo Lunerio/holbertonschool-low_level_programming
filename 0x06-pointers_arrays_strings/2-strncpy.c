1ecc967c7ade23e5ae33e738fb075cd871ae1699
Lunerio <lunerio@outlook.es>
2020-10-01 16:47:19 +0000

    repair 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -22,7 +22,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (count = 0; count <= index; count++)
 	{
 		dest[count] = src[count];
-		if (count == n - 1)
+		if (count == n)
 		{
 			break;
 		}
