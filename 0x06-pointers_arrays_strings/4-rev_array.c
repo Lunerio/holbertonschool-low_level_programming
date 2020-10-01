61ce73f57acf73f9aec4519f756e889e39c422af
Lunerio <lunerio@outlook.es>
2020-10-01 19:53:05 +0000

    add 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,46 +8,15 @@
  *
  * Return: nothing.
  */
-void print_array(int *a, int n)
-{
-	int i;
-
-	i = 0;
-	while (i < n)
-	{
-		if (i != 0)
-		{
-			printf(", ");
-		}
-		printf("%d", a[i]);
-		i++;
-	}
-	printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, sizeof(a) / sizeof(int));
-	print_array(a, sizeof(a) / sizeof(int));
-	return (0);
-}
 
 void reverse_array(int *a, int n)
 {
 	int temp;
-	int max = n;
+	int max = n - 1;
 	int min = 0;
 	int c;
 
-	for (c = 0; c < (n / 2); c++)
+	for (c = 0; c < n / 2; c++)
 	{
 		temp = a[max];
 		a[max] = a[min];
