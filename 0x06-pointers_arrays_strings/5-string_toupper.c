12634115e9507707d9447593dacb663745b4dd5e
Lunerio <lunerio@outlook.es>
2020-10-01 19:20:59 +0000

    add files

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,20 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * string_toupper - check the code for Holberton School students.
+ * @c: pointer
  * Return: Always 0.
  */
-int main(void)
-{
-	char s[] = "Holberton School!\n";
-	char *p;
-
-	p = string_toupper(s);
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
-}
 
 char *string_toupper(char *c)
 {
