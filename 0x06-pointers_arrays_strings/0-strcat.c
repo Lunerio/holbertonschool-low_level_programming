9cabcc8c3ce416560c0bab7cb232ba84ee631f10
Lunerio <lunerio@outlook.es>
2020-10-01 13:33:41 +0000

    add 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,24 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strcat - check the code for Holberton School students.
+ * @dest: pointer to dest
+ * @src: pointer to src
  * Return: Always 0.
  */
-int main(void)
-{
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *p;
-
-	printf("%s\n", s1);
-	printf("%s", s2);
-	p = _strcat(s1, s2);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", p);
-	return (0);
-}
 
 char *_strcat(char *dest, char *src)
 {
