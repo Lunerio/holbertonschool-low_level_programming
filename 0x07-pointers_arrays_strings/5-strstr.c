7860c84b4b5d4e39a1db7fe2900638da4fd9ac86
Lunerio <lunerio@outlook.es>
2020-10-05 21:45:22 +0000

    add 6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,11 +17,6 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; haystack[j] != '\0'; j++)
 		{
-			if (needle[i] == haystack[j])
-			{
-				return (&haystack[j]);
-			}
 		}
-	}
-	return ('\0');
+
 }
