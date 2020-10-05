02127df9b4fe6ac9e3ee3e21990e1033c0a76417
Lunerio <lunerio@outlook.es>
2020-10-05 20:53:28 +0000

    repair 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,17 +8,6 @@
  * Return: Always 0.
  */
 
-int main(void)
-{
-	char *s = "hello, world";
-	char *f = "world";
-	char *t;
-
-	t = _strpbrk(s, f);
-	printf("%s\n", t);
-	return (0);
-}
-
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
