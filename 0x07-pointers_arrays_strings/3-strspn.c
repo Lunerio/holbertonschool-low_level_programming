78064db1e579b086c4caa95ec3d49843af39e239
Lunerio <lunerio@outlook.es>
2020-10-05 15:38:20 +0000

    repair 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,33 +2,28 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * strspn - check the code for Holberton School students.
+ * @s: pointer to string
+ * @accept: pointer to string
  * Return: Always 0.
  */
-int main(void)
-{
-	char *s = "hello, world";
-	char *f = "oleh";
-	unsigned int n;
-
-	n = _strspn(s, f);
-	printf("%u\n", n);
-	return (0);
-}
 
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i;
 	unsigned int j;
+	unsigned int k = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < i; j++)
-		if (s == accept)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			break;
+			if (s[i] == accept[j])
+			{
+				k++;
+				break;
+			}
 		}
 	}
-	return (j);
+	return (k);
 }
