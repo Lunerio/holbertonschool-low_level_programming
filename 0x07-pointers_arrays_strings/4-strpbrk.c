206722532802037572418e2a7b5fb45a8ec3342d
Lunerio <lunerio@outlook.es>
2020-10-05 20:52:59 +0000

    repair 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,28 +8,32 @@
  * Return: Always 0.
  */
 
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = _strpbrk(s, f);
+	printf("%s\n", t);
+	return (0);
+}
+
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
 	int j;
 
-	for (i = 0; s[i] != '\0'; s++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+				break;
+			}
 		}
-		if (s[i] == accept[j])
-		{
-			break;
-		}
 	}
-	if (s[i] == accept[j])
-	{
-		return (s);
-	}
-	else
-	{
-		return (0);
-	}
-
+	return ('\0');
 }
