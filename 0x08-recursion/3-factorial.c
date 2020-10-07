5c20a47b66b37f47c290dee5d30c3d5b1351ee05
Lunerio <lunerio@outlook.es>
2020-10-07 18:32:20 +0000

    add 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * factorial - check the code for Holberton School students.
+ * @n: number
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
