2ea60e6b6def88242552ef5688144e16cc86780d
Lunerio <lunerio@outlook.es>
2020-10-10 18:43:54 +0000

    repair files

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,6 +1,5 @@
 #ifndef holberton_h
 #define holberton_h
-#endif
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,3 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
