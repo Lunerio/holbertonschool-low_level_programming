ee467bdf864bfbe33c38fb418cb4856004d16508
Lunerio <lunerio@outlook.es>
2020-10-15 18:13:44 +0000

    repair 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,56 +1,36 @@
 #include "holberton.h"
 #include <stdlib.h>
-/**
- * string_nconcat - return the concatenation
- * @s1: pointer to char
- * @s2: pointer to char
- * @n: limit
- * Return: pointer to nstr
- */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i1;
 	unsigned int i2;
-	char *nstr;
+	char *h;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
 	for (i1 = 0; s1[i1] != 0; i1++)
 	{
 	}
-	for (i2 = 0; s2[i2] != 0; i2++)
+	for (i2 = 0; s2[i2] != 0 && i2 < n; i2++)
 	{
+		i1++;
 	}
-	if (n >= i2)
-	{
-		nstr = malloc((i1 + i2) * sizeof(char) + 1);
-	}
-	else
-	{
-		nstr = malloc((i1 + n) * sizeof(char) + 1);
-	}
-	if (nstr == NULL)
+
+	h = malloc(i1 * sizeof(char) + 1);
+	if (h == NULL)
 	{
 		return (NULL);
 	}
+
 	for (i1 = 0; s1[i1] != 0; i1++)
 	{
-		nstr[i1] = s1[i1];
+		h[i1] = s1[i1];
 	}
-	for (i2 = 0; s2[i2] != 0; i2++)
+	for (i2 = 0; s2[i2] != 0 && i2 < n; i2++)
 	{
-		if (i2 == n)
-		{
-			break;
-		}
-		nstr[i1] = s2[i2];
+		h[i1] = s2[i2];
 		i1++;
 	}
-	return (nstr);
+
+	h[i1] = '\0';
+	return (h);
 }
