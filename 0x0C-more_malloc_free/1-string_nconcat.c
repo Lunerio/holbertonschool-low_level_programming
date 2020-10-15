dd7550c612cc0d6f0adbf15081d9c394efa6a3ca
Lunerio <lunerio@outlook.es>
2020-10-15 18:24:23 +0000

    repair 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i2;
 	char *h;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	for (i1 = 0; s1[i1] != 0; i1++)
 	{
 	}
@@ -34,9 +43,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (i2 = 0; s2[i2] != 0 && i2 < n; i2++)
 	{
-		h[i1 + i2] = s2[i2];
+		h[i1] = s2[i2];
+		i1++;
 	}
 
-	h[i1 + i2] = '\0';
+	h[i1] = '\0';
 	return (h);
 }
