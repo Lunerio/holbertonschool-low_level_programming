905cf0a533904d90b3dafa1db05ea219082c50b8
Lunerio <lunerio@outlook.es>
2020-10-15 18:17:46 +0000

    repair 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,14 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * string_nconcat - concat strings with n as limit
+ * @s1: string 1
+ * @s2: string 2
+ * @n: limit for s2
+ * Return: pointer
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i1;
@@ -12,10 +20,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (i2 = 0; s2[i2] != 0 && i2 < n; i2++)
 	{
-		i1++;
 	}
 
-	h = malloc(i1 * sizeof(char) + 1);
+	h = malloc((i1 + i2) * sizeof(char) + 1);
 	if (h == NULL)
 	{
 		return (NULL);
