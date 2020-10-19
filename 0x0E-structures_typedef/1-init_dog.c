3d9147dac35e735cbcaf5ec96d785c5ed97ad570
Lunerio <lunerio@outlook.es>
2020-10-19 19:39:07 +0000

    repair file 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * init_dog - init struc function
+ * @d: pointer to struct
+ * @name: pointer to char
+ * @age: pointer to int
+ * @owner: pointer to char
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
