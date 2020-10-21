efb705ebe07f1654121d2da9df0fec32f640c55e
Lunerio <lunerio@outlook.es>
2020-10-21 21:46:23 +0000

    add file 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,20 +1,29 @@
 #include <stdlib.h>
 
+/**
+ * int_index - returns the index of the value that matches
+ * @array: array given to analyze
+ * @size: size of array
+ * @cmp: function that analyzes
+ * Return: -1 if size <= 0, or if there are not matches. index if matches
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 	{
 		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
 	{
-		if ((*cmp)(array[i]) == array[i])
+		if ((*cmp)(array[i]))
 		{
 			return (i);
 		}
 	}
+
 	return (-1);
 }
