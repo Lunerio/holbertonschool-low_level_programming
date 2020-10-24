32ee105a643302334d53d25a5a29e219c63da3e3
Lunerio <lunerio@outlook.es>
2020-10-24 15:16:54 +0000

    repair 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,30 +14,29 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list hint;
 
-	if (n == 0)
+	if (n != 0)
 	{
-		return;
-	}
-
-	va_start(hint, n);
-	if (separator != NULL)
-	{
-		for (i = 0; i < n; i++)
+		va_start(hint, n);
+		if (separator != NULL)
 		{
-			printf("%d", va_arg(hint, int));
-			if (i < (n - 1))
+			for (i = 0; i < n; i++)
 			{
-				printf("%s", separator);
+				printf("%d", va_arg(hint, int));
+				if (i < (n - 1))
+				{
+					printf("%s", separator);
+				}
+			}
+		}
+		else
+		{
+			for (i = 0; i < n; i++)
+			{
+				printf("%u", va_arg(hint, int));
 			}
 		}
 	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%u", va_arg(hint, int));
-		}
-	}
+
 	va_end(hint);
 
 	printf("\n");
