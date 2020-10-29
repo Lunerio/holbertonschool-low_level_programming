cf62b5ebfa5f5835e7d4fddaa19ffc8d2a9670e4
Lunerio <lunerio@outlook.es>
2020-10-29 19:36:15 +0000

    add comments 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_node - add new node on the header
+ * @head: pointer to head position
+ * @str: string to add to the node
+ * Return: position of new node
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode;
