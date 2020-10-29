d90a0e3a39a072cc58f7e0a25d71e60bbe94ef2a
Lunerio <lunerio@outlook.es>
2020-10-29 22:57:05 +0000

    add file 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_list - free the memory allocated for each node
+ * @head: pointer to the header
+ */
+
+void free_list(list_t *head)
+{
+	if ((*head).next != NULL)
+	{
+		free_list((*head).next);
+	}
+	free(head);
+}
