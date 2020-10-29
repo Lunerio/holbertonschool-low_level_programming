9d5d2a90e834c080b42703e55cd4fba018de16d2
Lunerio <lunerio@outlook.es>
2020-10-29 18:11:17 +0000

    repair 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * list_len - List elements of list
+ * @h: pointer to elements of list
+ * Return: counter of elements of list
+
 size_t list_len(const list_t *h)
 {
 	unsigned int count = 0;
