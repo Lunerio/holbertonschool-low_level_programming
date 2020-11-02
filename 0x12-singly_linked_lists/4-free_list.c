2fa609893b98ab30af31912f45a90305f310ed72
Lunerio <lunerio@outlook.es>
2020-11-02 22:41:02 +0000

    asd

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- * free_list - free the memory allocated for each node
- * @head: pointer to the header
+ * free_list - check the code for Holberton School students.
+ * @head: pointer to pointer
+ * Return: Always 0.
  */
 
 void free_list(list_t *head)
