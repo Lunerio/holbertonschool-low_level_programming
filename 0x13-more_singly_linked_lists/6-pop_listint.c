fda99ffca87dea145d7ef4ce8a13a77e4c05e172
Lunerio <lunerio@outlook.es>
2020-11-04 17:50:09 +0000

    add file 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Remove the first node
+ * @head: pointer to head node
+ * Return: n of the node
+ */
+
+int pop_listint(listint_t **head)
+{
+	int save;
+	listint_t *aux = *head;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	save = aux->n;
+	*head = aux->next;
+	free(aux);
+	return (save);
+}
