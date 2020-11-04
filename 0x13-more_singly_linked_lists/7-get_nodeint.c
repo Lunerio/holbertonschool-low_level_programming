3bafd95aa45066e4eaef2b3c7c8e9bfd88e2aa6b
Lunerio <lunerio@outlook.es>
2020-11-04 18:48:35 +0000

    add file 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < index; i++)
+		{
+			head = head->next;
+		}
+	}
+	return (head);
+}
