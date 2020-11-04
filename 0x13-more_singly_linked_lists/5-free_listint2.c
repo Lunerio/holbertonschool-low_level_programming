e909d623e7ee7cfa722d83ffb231abca996bb1b2
Lunerio <lunerio@outlook.es>
2020-11-04 13:44:29 +0000

    add file 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free each node from memory and set the head pointer to NULL
+ * @head: pointer to first element
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+
+	aux = *head;
+
+	while (aux)
+	{
+		aux = aux->next;
+		free(aux);
+	}
+	*head = NULL;
+}
