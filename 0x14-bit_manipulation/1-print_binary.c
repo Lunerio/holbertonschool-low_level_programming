c2a6e515c930cf86420a27ed86cd20d545d6f8f3
Lunerio <lunerio@outlook.es>
2020-11-06 15:14:40 +0000

    repair 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,19 +2,26 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 32768;
+	long int i = 0;
+	long int mask = 0;
+	long int flag = 0;
 
-	while(mask > 1)
+	if (n == 0)
+		_putchar('0');
+
+	for (i = 32; i >= 0; i--)
 	{
-		if((n & mask) == 0)
-		{
-			_putchar('0');
+		mask = n >> i;
 
-		}
-		else
+		if (mask & 1)
 		{
+			flag = 1;
 			_putchar('1');
 		}
-		mask = mask >> 1;
+		else
+			if (flag == 1)
+			{
+				_putchar('0');
+			}
 	}
 }
