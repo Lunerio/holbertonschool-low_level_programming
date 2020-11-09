412fd353b1aa9b557ef84c093a9b5ace1c4dc3d1
Lunerio <lunerio@outlook.es>
2020-11-09 20:17:15 +0000

    repair 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,9 @@ int create_file(const char *filename, char *text_content)
 	int p;
 	int i;
 
-	for (i = 0; text_content[i] != '\0'; i++)
+	if (filename == NULL)
 	{
+		return (-1);
 	}
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
@@ -25,16 +26,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
+		for (i = 0; text_content[i] != 0; i++)
+		{
+		}
 		p = write(fd, text_content, i);
-	}
-	if (p == -1)
-	{
-		return (-1);
 	}
 
 	close(fd);
 
-	if (filename == NULL)
+	if (p == -1)
 	{
 		return (-1);
 	}
