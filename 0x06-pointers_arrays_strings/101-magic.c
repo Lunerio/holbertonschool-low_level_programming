286a2e3e51b983fbf20d0c54c99ee2615446adba
Lunerio <lunerio@outlook.es>
2020-11-29 03:39:59 +0000

    repair 101

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -16,8 +16,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	*(p + 5) = 98; /*moves pointer 5 positions from the n declaration*/
-/*accessing the position 2 of array a/*
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
