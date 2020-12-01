a453e439541a3f5797d854a385361676a4cc23ab
Lunerio <lunerio@outlook.es>
2020-12-01 19:34:14 +0000

    add ex 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * main - duh
+ * @ac: argument count
+ * @av: list of arguments
+ * Return: 0;
+ */
+
+int main(int ac, char **av)
+{
+	int a;
+	int b;
+	int ret;
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(av[2])(a, b) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(av[1]);
+	b = atoi(av[3]);
+	ret = get_op_func(av[2])(a, b);
+	printf("%d\n", ret);
+
+	return (0);
+}
