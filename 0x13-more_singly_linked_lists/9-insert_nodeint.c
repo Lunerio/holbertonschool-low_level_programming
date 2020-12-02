0c0da7fc0d439da8482d1746268f29aed33647b6
Lunerio <lunerio@outlook.es>
2020-12-02 20:53:44 +0000

    repair file 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (*head == NULL || idx == 0)
 	{
-		newnode->next = NULL;
+		newnode->next = *head;
 		*head = newnode;
 		return (newnode);
 	}
