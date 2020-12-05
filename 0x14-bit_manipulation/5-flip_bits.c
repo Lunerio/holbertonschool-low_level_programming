160aafd21bbfbee3d5f6e875e5b3293b016b82ad
Lunerio <lunerio@outlook.es>
2020-12-05 17:38:54 +0000

    add file 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - count how many bits need to change
+ * @n: first number
+ * @m: second number
+ * Return: Numbers of bits changed
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned long int comp;
+
+	comp = n ^ m;
+	while (comp != 0)
+	{
+		if ((comp & 1) == 1)
+		{
+			count ++;
+		}
+		comp = comp >> 1;
+	}
+	return (count);
+}
