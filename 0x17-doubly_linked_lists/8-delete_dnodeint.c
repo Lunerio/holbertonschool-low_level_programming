fe70b269a08802ef58450b93d59a078e7ab2139b
Lunerio <lunerio@outlook.es>
2020-12-12 19:34:53 +0000

    first push 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes any node at given index
+ * @head: pointer to first element
+ * @index: index to delete
+ * Return: 1 on success, -1 on failure
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+	dlistint_t *prev_node;
+	dlistint_t *next_node;
+	dlistint_t *check;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (current->prev != NULL)
+	{
+		while (current->prev != NULL)
+			current = current->prev;
+	}
+	if (index == 0)
+	{
+		next_node = current->next;
+		*head = next_node;
+		next_node->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	for (i = 0; i < index; i++)
+	{
+		check = current->next;
+		if (check == NULL)
+			return (-1);
+		current = current->next;
+	}
+	prev_node = current->prev;
+	next_node = current->next;
+	prev_node->next = next_node;
+	next_node->prev = prev_node;
+	free(current);
+	return (1);
+}
