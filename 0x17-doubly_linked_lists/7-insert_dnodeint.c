9f872de06f13bcb143d7c1a77b1f4bc5fdbcdfd2
Lunerio <lunerio@outlook.es>
2020-12-13 20:39:52 +0000

    repair 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,12 +25,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new = add_dnodeint(h, n);
 		return (new);
 	}
+	if (idx >= dlistint_len(current))
+		return (NULL);
 	current = *h;
-	if (current->prev != NULL)
-	{
-		while (current->prev != NULL)
-			current = current->prev;
-	}
 	for (i = 0; i < idx; i++)
 	{
 		if (current->next == NULL)
@@ -44,10 +41,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n; /**/
-	current->prev = new; /**/
-	new->next = current; /**/
-	new->prev = previous; /**/
-	previous->next = new; /**/
+	new->n = n;
+	current->prev = new;
+	new->next = current;
+	new->prev = previous;
+	previous->next = new;
 	return (new);
+}
+
+/**
+ * dlistint_len - get number of elements in a doubly linked list
+ * @h: pointer to first element
+ * Return: Number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	if (h->prev != NULL)
+	{
+		while (h->prev != NULL)
+		{
+			h = h->prev;
+		}
+	}
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
