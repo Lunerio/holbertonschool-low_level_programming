96375ee996bf26b0d83e505a3ef544a98397a4d6
Lunerio <lunerio@outlook.es>
2020-12-13 03:51:59 +0000

    repair 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,8 +12,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current = *head;
 	dlistint_t *prev_node;
 	dlistint_t *next_node;
-	dlistint_t *check;
 	unsigned int i;
+	unsigned int num;
 
 	if (*head == NULL)
 		return (-1);
@@ -32,12 +32,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(current);
 		return (1);
 	}
-
+	num = dlistint_len(current);
 	for (i = 0; i < index; i++)
 	{
-		check = current->next;
-		if (check == NULL)
-			return (-1);
+		if (index > num)
+		{
+			free(current);
+			return (1);
+		}
 		current = current->next;
 	}
 	prev_node = current->prev;
@@ -46,4 +48,33 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	next_node->prev = prev_node;
 	free(current);
 	return (1);
+}
+
+/**
+ * dlistint_len - get number of elements in a doubly linked list
+ * @h: pointer to first element
+ * Return: Number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	if (h->prev != NULL)
+	{
+		while (h->prev != NULL)
+		{
+			h = h->prev;
+		}
+	}
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
