d3657c335d5324e19545caf5cb25de855ecfabc3
Lunerio <lunerio@outlook.es>
2020-12-13 16:24:21 +0000

    test 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	if (index == 0)
 	{
-		if ((*head->next != NULL))
+		if ((*head)->next != NULL)
 		{
 			*head = current->next;
 			(*head)->prev = NULL;
