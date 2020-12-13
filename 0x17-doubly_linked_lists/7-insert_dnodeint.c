485505c72889b3cbc2f867d759a6ac4631cbdedd
Lunerio <lunerio@outlook.es>
2020-12-13 05:03:56 +0000

    repair 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,8 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 	{
-		new = add_dnodeint_end(h, n);
-		return (new);
+		return (NULL);
 	}
 	if (idx == 0)
 	{
@@ -43,10 +42,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	current->prev = new;
-	new->next = current;
-	new->prev = previous;
-	previous->next = new;
+	new->n = n; /**/
+	current->prev = new; /**/
+	new->next = current; /**/
+	new->prev = previous; /**/
+	previous->next = new; /**/
 	return (new);
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i;
 
 	if (*head == NULL)
-		return (-1);
+		return (NULL);
 
 	if (current->prev != NULL)
 	{
