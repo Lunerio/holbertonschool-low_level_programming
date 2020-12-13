aa49dcf9f34392f15c99d18f77545c01a91b1edc
Lunerio <lunerio@outlook.es>
2020-12-13 04:42:02 +0000

    repair 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,8 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (current->next == NULL)
 		{
-			new = add_dnodeint_end(h, n);
-			return (new);
+			return (NULL);
 		}
 		current = current->next;
 	}
