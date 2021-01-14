569a65713473a3b6622b48e41b3bf1b5bf6d424d
Lunerio <lunerio@outlook.es>
2021-01-14 20:47:55 +0000

    add file 3 finished

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -33,16 +33,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	size = ht->size;
 	index = key_index(new_key, size);
 
-/*	if (ht->array[index] != NULL)
+	if (ht->array[index] != NULL)
 	{
-		agregar nodo nuevo en caso de que no este vacio
+		newnode->key = (char*)new_key;
+		newnode->value = new_value;
+		newnode->next = ht->array[index];
 	}
- lo siguiente ponerlo en un else
-*/
-	newnode->key = (char*)new_key;
-	newnode->value = new_value;
-	newnode->next = NULL;
-
+	else
+	{
+		newnode->key = (char*)new_key;
+		newnode->value = new_value;
+		newnode->next = NULL;
+	}
 	ht->array[index] = newnode;
 
 	return (1);
