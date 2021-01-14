55157d006086c53213e960f199aedeff69c2742d
Lunerio <lunerio@outlook.es>
2021-01-14 22:48:25 +0000

    save 4

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,29 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i = 0;
+
+	if (size == 0)
+		return (NULL);
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
+
 	(table)->size = size;
-	(table)->array = malloc(sizeof(hash_table_t **) * size);
+
+	(table)->array = malloc(8 * size);
 	if ((table)->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
+
+	while (i < size)
+	{
+		(table)->array[i] = NULL;
+		i++;
+	}
 
 	return (table);
 }
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - return the value of a given key(the first one)
+ * @ht: hash table to check
+ * @key: key to get
+ * Return: Pointer of char, or NULL in case there's no such key or error
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	const unsigned char *new_key;
+	unsigned long int size;
+
+	if (ht == NULL)
+		return (NULL);
+	if (key == NULL || strcmp(key, "") == 0)
+		return (NULL);
+
+	new_key = (unsigned char*)key;
+	size = ht->size;
+	index = key_index(new_key, size);
+
+	return (key_value);
+}
