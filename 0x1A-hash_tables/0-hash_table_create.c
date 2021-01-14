950e62c285b7f1958e1ba21605dd42cb9fca3fe0
Lunerio <lunerio@outlook.es>
2021-01-14 17:57:46 +0000

    first push

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table. returns pointer to table
+ * @size: size of the array
+ * Return: pointer to the new table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return NULL;
+	(table)->size = size;
+	(table)->array = malloc(sizeof(hash_table_t **) * size);
+
+	return (table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+Creating and adding elements to hash tables
