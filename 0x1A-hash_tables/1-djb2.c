6456806a36b580d98c17e454d9e759a15be5aef6
Lunerio <lunerio@outlook.es>
2021-01-15 03:17:42 +0000

    repair betty for 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - function to get a hash number from a character
+ * @str: string to analyze
+ * Return: hash number
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
