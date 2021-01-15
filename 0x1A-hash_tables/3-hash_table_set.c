23c55b1d4f4926e0ac6190152afff4286c2fb387
Lunerio <lunerio@outlook.es>
2021-01-15 16:35:44 +0000

    que pasa con el 3?

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,7 +29,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(save->key, key) == 0)
 		{
-			free(save->value);
 			save->value = strdup(value);
 			return (1);
 		}
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,17 +10,13 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned int index;
-	const unsigned char *new_key;
-	unsigned long int size;
 
 	if (ht == NULL)
 		return (NULL);
 	if (key == NULL || strcmp(key, "") == 0)
 		return (NULL);
 
-	new_key = (unsigned char *)key;
-	size = ht->size;
-	index = key_index(new_key, size);
+	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
 
@@ -33,10 +29,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 			return (ht->array[index]->value);
 		if (ht->array[index]->next == NULL)
 			break;
-		printf("antes de moverse\n");
 		ht->array[index] = ht->array[index]->next;
-		printf("luego de moverse\n");
 	}
-
 	return (NULL);
 }
