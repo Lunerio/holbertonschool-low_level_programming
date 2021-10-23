986dbc17c321c6a5f00a45efac7a74bcd55334e3
hugo-gomez <hugo.gomez@globant.com>
2021-10-23 10:10:08 -0300

    Dale betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,29 +9,33 @@
  * Return: count of coins
  */
 
-int main(int argc, char **argv) {
-    
-    int coins[5] = {25, 10, 5, 2, 1};
-    int sum = 0;
-    int money;
-    int i;
+int main(int argc, char **argv)
+{
+		
+	int coins[5] = {25, 10, 5, 2, 1};
+	int sum = 0;
+	int money;
+	int i;
 
-    if (argc != 2) {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2) {
+		printf("Error\n");
+		return (1);
+	}
 
-    money = atoi(argv[1]);
+	money = atoi(argv[1]);
 
-    for (; money > 0;) {
-        for (i = 0; i < 5; i++) {
-            if (money - coins[i] > 0 || money % coins[i] == 0) {
-                sum++;
-                money -= coins[i];
-                break;
-            }
-        }
-    }
-    printf("%d\n", sum);
-    return (0);
+	for (; money > 0;)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (money - coins[i] > 0 || money % coins[i] == 0)
+			{
+				sum++;
+				money -= coins[i];
+				break;
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
